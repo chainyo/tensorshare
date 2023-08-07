7846c0a6b7554be82c600f4f1202f80ede246ccb
Thomas Chaigneau <t.chaigneau.tc@gmail.com>
2023-08-07 16:50:30 +0200

    leverage ByteSize (#6)
    Fixes #3

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -1,6 +1,7 @@
 """Tests for the pydantic schemas of tensorshare."""
 
 import pytest
+from pydantic import ByteSize
 
 from tensorshare.converter.utils import (
     convert_flax_to_safetensors,
@@ -15,9 +16,47 @@ from tensorshare.schema import TensorShare
 class TestTensorShare:
     """Tests for the pydantic schemas of tensorshare."""
 
-    def test_tensorshare_pydantic_schema(self) -> None:
+    @pytest.mark.usefixtures("converted_fixed_numpy_tensors")
+    def test_tensorshare_pydantic_schema_with_fixed_tensor(
+        self, converted_fixed_numpy_tensors
+    ) -> None:
         """Test the pydantic schema of tensorshare."""
-        # TODO: Check the TensorShare pydantic schema and its model_config
+        tensorshare = TensorShare(
+            tensors=converted_fixed_numpy_tensors,
+            size=len(converted_fixed_numpy_tensors),
+        )
+
+        assert isinstance(tensorshare.tensors, bytes)
+        assert isinstance(tensorshare.size, int)
+
+        assert isinstance(tensorshare.size_as_str, str)
+        assert tensorshare.size_as_str == "112B"
+
+        assert (
+            tensorshare.model_dump_json(indent=4)
+            == '{\n    "tensors":'
+            ' "SAAAAAAAAAB7ImVtYmVkZGluZ3MiOnsiZHR5cGUiOiJGNjQiLCJzaGFwZSI6WzIsMl0sImRhdGFfb2Zmc2V0cyI6WzAsMzJdfX0gICAgICAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA==",\n'
+            '    "size": 112\n}'
+        )
+
+    @pytest.mark.usefixtures("random_fp32_numpy_tensors")
+    def test_tensorshare_pydantic_schema_with_random_tensor(
+        self, random_fp32_numpy_tensors
+    ) -> None:
+        """Test the pydantic schema of tensorshare."""
+        converted_tensors = convert_numpy_to_safetensors(random_fp32_numpy_tensors)
+
+        tensorshare = TensorShare(
+            tensors=converted_tensors,
+            size=len(converted_tensors),
+        )
+
+        assert tensorshare.tensors == converted_tensors
+        assert tensorshare.size == len(converted_tensors)
+
+        assert (
+            tensorshare.size_as_str == ByteSize(len(converted_tensors)).human_readable()
+        )
 
     @pytest.mark.usefixtures("dict_zeros_flax_tensor")
     def test_tensorshare_with_flax(self, dict_zeros_flax_tensor) -> None:
