418dbf6feaa6ef8d593973dfe26017412ed4b70d
chainyo <t.chaigneau.tc@gmail.com>
2023-08-12 13:23:31 +0200

    simplify by using only one client

--- src/tensorshare/client/client.py
+++ src/tensorshare/client/client.py
@@ -1,14 +1,62 @@
-"""Sync http client for sending tensors to a remote server."""
+"""Async http client for sending tensors to a remote server."""
 
-import requests
+import asyncio
 
-from tensorshare.client.base import TensorShareClient
+import aiohttp
+from pydantic import BaseModel, HttpUrl, ValidationError
+
 from tensorshare.client.utils import fake_tensorshare_data
 from tensorshare.schema import TensorShare
 
 
-class SyncTensorShareClient(TensorShareClient):
-    """Synchronous Client for sending tensors to a remote server."""
+class TensorShareServer(BaseModel):
+    """ServerUrl model."""
+
+    url: HttpUrl
+    ping: HttpUrl
+    receive_tensor: HttpUrl
+
+
+class TensorShareClient:
+    """Asynchronous Client for sending tensors to a remote server.
+
+    This client uses the aiohttp library which is an asynchronous http client,
+    built on top of asyncio. But the client provides a synchronous interface
+    for the users through the use of the `asyncio.run` function.
+
+    Note:
+        Using the synchronous interface will block the main thread until the
+        request is completed and the response is received. It's not recommended
+        to use the synchronous interface in a production environment as it
+        may cause performance issues. The synchronous interface is only
+        provided for testing purposes.
+
+    Example:
+        >>> import asyncio
+        >>> import torch
+        >>> from tensorshare import TensorShare, TensorShareClient
+
+        >>> client = TensorShareClient("http://localhost:8765")
+        >>> ts = TensorShare.from_dict({"embeddings": torch.rand(10, 10)})
+
+        >>> # Synchronous interface
+        >>> print(client.ping_server())
+        >>> # True
+
+        >>> print(client.send_tensor(ts))
+        >>> # <ClientResponse(http://localhost:8765/receive_tensor) [200 OK]>    
+
+        >>> # Asynchronous interface
+        >>> async def main():
+        ...     r = await client.async_ping_server()
+        ...     print(r)
+        ...     r = await client.async_send_tensor(ts)
+        ...     print(r)
+
+        >>> asyncio.run(main())
+        >>> # True
+        >>> # <ClientResponse(http://localhost:8765/receive_tensor) [200 OK]>
+    """
 
     def __init__(self, server_url: str, timeout: int = 10) -> None:
         """Initialize the client.
@@ -22,25 +70,54 @@ class SyncTensorShareClient(TensorShareClient):
         Raises:
             ValueError: If the server_url is not a valid url.
         """
-        super().__init__(server_url, timeout)
+        try:
+            self.server = TensorShareServer(
+                url=HttpUrl(server_url),
+                ping=HttpUrl(f"{server_url}/ping"),
+                receive_tensor=HttpUrl(f"{server_url}/receive_tensor"),
+            )
+        except ValidationError as e:
+            raise ValueError(
+                f"The provided server url is invalid -> {server_url}\n{e}"
+            ) from e
+
+        self.timeout = timeout
+
+        self._validate_endpoints()
 
     def ping_server(self) -> bool:
         """Ping the server to check if it is available."""
-        with requests.Session() as session:
-            response = session.get(str(self.server.ping), timeout=self.timeout)
+        return asyncio.run(self.async_ping_server())
 
-        return response.status_code == 200
+    async def async_ping_server(self) -> bool:
+        """Ping the server to check if it is available."""
+        async with aiohttp.ClientSession() as session:
+            response = await session.get(str(self.server.ping), timeout=self.timeout)
 
-    def send_tensor(self, tensor_data: TensorShare) -> requests.Response:
-        """
-        Send a TensorShare object to the server using requests.
+        return response.status == 200
+
+    def send_tensor(self, tensor_data: TensorShare) -> aiohttp.ClientResponse:
+        """Send a TensorShare object to the server using aiohttp.
 
         Args:
             tensor_data (TensorShare):
                 The tensor data to send to the server.
 
         Returns:
-            requests.Response: The response from the server.
+            aiohttp.ClientResponse: The response from the server.
+        """
+        return asyncio.run(self.async_send_tensor(tensor_data))
+
+    async def async_send_tensor(self, tensor_data: TensorShare) -> aiohttp.ClientResponse:
+        """
+        Send a TensorShare object to the server using aiohttp.
+
+        Args:
+            tensor_data (TensorShare):
+                The tensor data to send to the server.
+
+        Returns:
+            aiohttp.ClientResponse: The response from the server.
         """
         if not isinstance(tensor_data, TensorShare):
             raise TypeError(
@@ -48,38 +125,56 @@ class SyncTensorShareClient(TensorShareClient):
                 f" {type(tensor_data)}"
             )
 
-        with requests.Session() as session:
-            response = session.post(
+        async with aiohttp.ClientSession() as session:
+            response = await session.post(
                 str(self.server.receive_tensor),
                 headers={"Content-Type": "application/json"},
                 data=tensor_data.model_dump_json(),
                 timeout=self.timeout,
             )
+            session.close()
 
         return response
 
-    async def _validate_endpoints(self) -> None:
+    def _validate_endpoints(self) -> None:
         """
-        Register the endpoints for the client to use.
+        Check that all the registered endpoints are available on the server.
 
         Raises:
-            ValueError: If the server is not available after the ping request.
+            AssertionError: If any of the endpoints are not available.
         """
         # TODO: Add logging to indicate which endpoint is being checked.
-        # Check the ping endpoint
-        self.ping_server()
-        # Check the receive_tensor endpoint
-        self.send_tensor(fake_tensorshare_data())
+        try:
+            # Check the ping endpoint
+            r = self.ping_server()
+            assert r is True
+
+            # Check the receive_tensor endpoint
+            r = self.send_tensor(fake_tensorshare_data())
+            assert r.status == 200
+
+        except Exception as e:
+            raise AssertionError(
+                f"Could not connect to the server at {self.server.url}\n{e}"
+            ) from e
+
 
 # import torch
 
 # from tensorshare.schema import TensorShare
 
 # ts = TensorShare.from_dict({"embeddings": torch.rand(10, 10)})
-# client = SyncTensorShareClient("http://localhost:8765")
-# # client = SyncTensorShareClient("htt:")
+# client = TensorShareClient("http://localhost:8765")
+# # client = AsyncTensorShareClient("htt:")
 
-# r = client.ping_server()
-# print(r)
-# r = client.send_tensor(ts)
-# print(r)
+# async def main():
+#     r = await client.async_ping_server()
+#     print(r)
+#     r = await client.async_send_tensor(ts)
+#     print(r)
+
+# import asyncio
+# asyncio.run(main())
+
+# print(client.ping_server())
+# print(client.send_tensor(ts))
