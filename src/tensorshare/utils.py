e153554d76d50c780c6e1439f933df9fcdc93b3a
chainyo <t.chaigneau.tc@gmail.com>
2023-08-19 23:33:24 +0200

    add prepare_tensors_to_dict

--- src/tensorshare/utils.py
+++ src/tensorshare/utils.py
@@ -1,5 +1,8 @@
 """Utility functions for the Client module."""
 
+import types
+from typing import Any, Dict
+
 from pydantic import ByteSize
 
 from tensorshare.schema import TensorShare
@@ -22,3 +25,36 @@ def fake_tensorshare_data() -> TensorShare:
         ),
         size=ByteSize(84),
     )
+
+
+def prepare_tensors_to_dict(data: Any) -> Dict[str, Any]:
+    """Prepare the tensors from any framework to a dictionary.
+
+    This function is used to prepare the tensors from any framework to a
+    dictionary. The dictionary will be used to create a TensorShare object.
+    Use this function as a lazy way to prepare the tensors.
+    If the data is a dictionary, the keys will be used as the tensor names.
+    If the data is a list, a set, a tuple or any other iterable, the tensors
+    will be named "embeddings_{i}" where i is the index of the tensor in the
+    iterable.
+
+    Args:
+        data (Any):
+            The tensors to prepare. It can be a dictionary, a generator,
+            a list, a set, a tuple or any other single item.
+
+    Returns:
+        Dict[str, Any]: The prepared data.
+    """
+    if isinstance(data, dict):
+        return {str(key): tensor for key, tensor in data.items()}
+
+    elif isinstance(data, (types.GeneratorType, list, set, tuple)) or hasattr(
+        data, "__iter__"
+    ):
+        # The `hasattr(data, "__iter__")` check ensures that even if the input is some other kind of iterable
+        # (not explicitly listed), it will still be processed here.
+        return {f"embeddings_{i}": tensor for i, tensor in enumerate(data)}
+
+    else:
+        return {"embeddings": data}
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,9 +1,12 @@
 """Test the utils functions of the Client module."""
 
+from typing import Any, Dict
+
+import pytest
 from pydantic import ByteSize
 
 from tensorshare.schema import TensorShare
-from tensorshare.utils import fake_tensorshare_data
+from tensorshare.utils import fake_tensorshare_data, prepare_tensors_to_dict
 
 
 class TestClientUtils:
@@ -20,3 +23,39 @@ class TestClientUtils:
             b"       \x00\x00\x00\x00"
         )
         assert fake_ts.size == ByteSize(84)
+
+    @pytest.mark.parametrize(
+        "data, expected",
+        [
+            ({"embeddings": [1, 2, 3]}, {"embeddings": [1, 2, 3]}),
+            (
+                {"embeddings": [1, 2, 3], "labels": [1, 2, 3]},
+                {"embeddings": [1, 2, 3], "labels": [1, 2, 3]},
+            ),
+            (
+                [[1, 2, 3], [1, 2, 3]],
+                {"embeddings_0": [1, 2, 3], "embeddings_1": [1, 2, 3]},
+            ),
+            (
+                ([1, 2, 3], [1, 2, 3]),
+                {"embeddings_0": [1, 2, 3], "embeddings_1": [1, 2, 3]},
+            ),
+            (
+                {0: [1, 2, 3], 1: [1, 2, 3]},
+                {"0": [1, 2, 3], "1": [1, 2, 3]},
+            ),
+            (
+                (lambda: (i for i in [[1, 2, 3], [1, 2, 3], [1, 2, 3]])),
+                {
+                    "embeddings_0": [1, 2, 3],
+                    "embeddings_1": [1, 2, 3],
+                    "embeddings_2": [1, 2, 3],
+                },
+            ),
+        ],
+    )
+    def test_prepare_tensors_to_dict(self, data: Any, expected: Dict[str, Any]) -> None:
+        """Test the prepare_tensors_to_dict function."""
+        data = data() if callable(data) else data
+
+        assert prepare_tensors_to_dict(data) == expected
