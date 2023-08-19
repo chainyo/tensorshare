6d47a01c9cdef4d9f9cb7a1bdd3074c13d4e664d
chainyo <t.chaigneau.tc@gmail.com>
2023-08-20 00:48:40 +0200

    add logging module

---
+++ src/tensorshare/logging.py
@@ -0,0 +1,27 @@
+"""Logging module for tensorshare."""
+
+import functools
+import logging
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+
+logger = logging.getLogger(__name__)
+
+
+# From the huggingface/transformers library
+# https://github.com/huggingface/transformers/blob/main/src/transformers/utils/logging.py#L297
+@functools.lru_cache(None)
+def warning_once(self, *args, **kwargs):
+    """
+    This method is identical to `logger.warning()`, but will emit the warning with the same message only once
+
+    Note: The cache is for the function arguments, so 2 different callers using the same arguments will hit the cache.
+    The assumption here is that all warning messages are unique across the code. If they aren't then need to switch to
+    another type of cache that includes the caller frame information in the hashing function.
+    """
+    self.warning(*args, **kwargs)
+
+
+logging.Logger.warning_once = warning_once
