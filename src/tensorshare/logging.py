44905dc300f38e75e3839cf0ba57a08f8d11ec5d
chainyo <t.chaigneau.tc@gmail.com>
2023-08-20 12:10:50 +0200

    fix logger

--- src/tensorshare/logging.py
+++ src/tensorshare/logging.py
@@ -1,27 +1,29 @@
 """Logging module for tensorshare."""
 
-import functools
 import logging
 
+
+class TensorShareLogger(logging.Logger):
+    """TensorShare logger class."""
+
+    _warning_cache = set()
+
+    def __init__(self, name: str, *args, **kwargs) -> None:
+        """Initialize the logger."""
+        super().__init__(name, *args, **kwargs)
+
+    def warning_once(self, *args, **kwargs) -> None:
+        """A warning method that only logs once."""
+        hash_key = hash(str(args) + str(kwargs))
+        if hash_key not in TensorShareLogger._warning_cache:
+            self.warning(*args, **kwargs)
+            TensorShareLogger._warning_cache.add(hash_key)
+
+
+logging.setLoggerClass(TensorShareLogger)
 logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
 )
 
 logger = logging.getLogger(__name__)
-
-
-# From the huggingface/transformers library
-# https://github.com/huggingface/transformers/blob/main/src/transformers/utils/logging.py#L297
-@functools.lru_cache(None)
-def warning_once(self, *args, **kwargs):
-    """
-    This method is identical to `logger.warning()`, but will emit the warning with the same message only once
-
-    Note: The cache is for the function arguments, so 2 different callers using the same arguments will hit the cache.
-    The assumption here is that all warning messages are unique across the code. If they aren't then need to switch to
-    another type of cache that includes the caller frame information in the hashing function.
-    """
-    self.warning(*args, **kwargs)
-
-
-logging.Logger.warning_once = warning_once
